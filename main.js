b8dc42c68121023a6391c8c38f036d52b02a8d61
debutler266 <32465329+debutler266@users.noreply.github.com>
2021-02-02 16:59:37 -0500

    pushing project to github

---
+++ main.js
@@ -0,0 +1,22 @@
+const reveal = gsap.utils.toArray('.reveal');
+reveal.forEach((text, i) => {
+  ScrollTrigger.create({
+    trigger: text,
+    toggleClass: 'active',
+    start: "top 90%",
+    end: "top 20%",
+  //  markers: true //hides start/end scroll controls
+  })
+})
+
+
+const images = gsap.utils.toArray('img');
+images.forEach((img, i) => {
+  ScrollTrigger.create({
+    trigger: img,
+    toggleClass: 'active',
+    start: "top 90%",
+    end: "top 20%",
+  //  markers: true
+  })
+})
